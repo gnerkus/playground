a3acbcab2e32eb16c5aa9057d80b8713085bbe8b
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2025-01-05 17:44:28 +0100

    compare with non-dynamic weights

--- algos/dynamic_random.js
+++ algos/dynamic_random.js
@@ -3,16 +3,17 @@ const weightIncrease = 1;
 const weightMultiple = 10;
 const initWeight = (size - weightIncrease) * weightMultiple;
 const weights = Array(size).fill(initWeight);
+const nonDynamicWeights = [...weights];
 const weightDecrease = size - weightIncrease;
 const totalWeight = size * initWeight;
 
-function weightedRandomSelect() {
+function weightedRandomSelect(isDynamic, listOfWeights) {
   let selected = -1;
 
   // 1. cummulative weights
   const cumulativeWeights = [];
-  for (let i = 0; i < weights.length; i += 1) {
-    cumulativeWeights[i] = weights[i] + (cumulativeWeights[i - 1] || 0);
+  for (let i = 0; i < listOfWeights.length; i += 1) {
+    cumulativeWeights[i] = listOfWeights[i] + (cumulativeWeights[i - 1] || 0);
   }
   // 2. get random number
   const randomNumber = totalWeight * Math.random();
@@ -25,15 +26,18 @@ function weightedRandomSelect() {
     }
   }
 
-  if (selected > -1) {
-    for (let i = 0; i < size; i++) {
-      if (i === selected) {
-        weights[i] -= weightDecrease;
-      } else {
-        weights[i] += weightIncrease;
+  if (isDynamic) {
+    if (selected > -1) {
+      for (let i = 0; i < size; i++) {
+        if (i === selected) {
+          listOfWeights[i] -= weightDecrease;
+        } else {
+          listOfWeights[i] += weightIncrease;
+        }
       }
     }
   }
+
 
   return selected;
 }
@@ -41,16 +45,19 @@ function weightedRandomSelect() {
 const freq = Object.fromEntries(Array(size).fill(0).map((item, index) => {
   return [index, item]
 }));
+const nonDynamicFreq = {...freq};
 
-for (let i = 0; i < 100; i++) {
-  console.log(`RUN NUMBER ${i + 1}:`);
-  console.log("init weights", weights);
-  const selected = weightedRandomSelect();
+for (let i = 0; i < size * 100; i++) {
+  const selected = weightedRandomSelect(true, weights);
+  const ndSelected = weightedRandomSelect(false, nonDynamicWeights);
   freq[selected] += 1;
-  console.log("selected index", selected);
-  console.log("new weights", weights);
+  nonDynamicFreq[ndSelected] += 1;
 }
 
+console.log(`final weights after ${size * 100} runs:`, weights);
+console.log("frequencies for dynamic weights:")
 console.log(freq);
+console.log("frequencies for non-dynamic weights")
+console.log(nonDynamicFreq);
 
 
